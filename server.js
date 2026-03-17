a3ab818eebf053834166a55ef1b4f5035e71ceda
Dhina7322 <dhina7322@gmail.com>
2026-03-17 15:54:04 +0530

    .

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ dotenv.config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    process.env.FRONTEND_URL,
+  ].filter(Boolean),
+  credentials: true,
+}));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
